2f4977d6aa56d7ca971cea362d89d095899d6938
Philipp Moers <soziflip@gmail.com>
2021-04-30 18:49:39 +0200

    Create App that renders list of team members

--- src/main/scala/App.scala
+++ src/main/scala/App.scala
@@ -1,68 +1,32 @@
 import ajax.AjaxTester
 import com.raquo.laminar.api.L._
-import oldstuff.intro.DuckMaster
-import oldstuff.pseudotests.SvgContainer
-import todomvc.TodoMvcApp
 import org.scalajs.dom.document
-import webcomponents.WebComponentsPage
 
 object App {
 
   def main(args: Array[String]): Unit = {
 
+    val teamMembers = List("Alex", "Armin", "Christof", "Luciano", "Nebo", "Philipp", "Sabrina")
+
+
     // This div, its id and contents are defined in index-fastopt.html and index-fullopt.html files
     lazy val container = document.getElementById("app-container")
 
-    lazy val appElement = {
-      val maybeChosenApp = Var[Option[Example]](None)
-
-      div(
-        children <-- maybeChosenApp.signal.map {
-          case Some(example) =>
-            example.render() :: Nil
-          case None =>
-            List(
-              h1("Choose example:"),
-              ul(
-                fontSize := "130%",
-                lineHeight := "2em",
-                examples.map { ex =>
-                  li(
-                    a(
-                      href := "#",
-                      ex.caption,
-                      onClick.preventDefault.mapTo(Some(ex)) --> maybeChosenApp.writer
-                    )
-                  )
-                }
-              ),
-              "Get back to this menu by reloading the page."
-            )
+    lazy val appElement = div(
+      h1("UES Daily Standup"),
+      ul(
+        fontSize := "130%",
+        lineHeight := "2em",
+        teamMembers.map { member =>
+          li(
+            span(member)
+          )
         }
-      )
-    }
+        ),
+    )
 
     // Wait until the DOM is loaded, otherwise app-container element might not exist
     renderOnDomContentLoaded(container, appElement)
   }
 
-  sealed abstract class Example(val caption: String, val render: () => HtmlElement)
-
-  case object TodoMVCExample extends Example("TodoMVC", () => TodoMvcApp())
-  case object AjaxExample extends Example("Ajax", () => AjaxTester())
-  case object WebComponentsExample extends Example("Web Components", () => WebComponentsPage())
-  case object SvgContainerExample extends Example("SVG Container", () => SvgContainer())
-  case object DuckCounterExample extends Example("Duck Counter", () => DuckMaster())
-  // case object ControlledValueTester extends Example("Controlled Value Tester", () => ControlledValue())
-  // case object ControlledCheckedTester extends Example("Controlled Checked Tester", () => ControlledChecked())
-
-  val examples: List[Example] = List(
-    DuckCounterExample,
-    TodoMVCExample,
-    AjaxExample,
-    WebComponentsExample,
-    SvgContainerExample,
-    // ControlledValueTester,
-    // ControlledCheckedTester
-  )
 }
